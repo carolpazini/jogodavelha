6b575147917e243b477e9367af23889e2e6ada86
Caroline Pazini Cavalcante - CCV <caroline.cavalcante@iteris.local>
2019-04-12 08:49:10 -0300

    limpeza inserida com sucesso

--- jogo.js
+++ jogo.js
@@ -1,21 +1,20 @@
 icone = "<img src = ankh.png>";
  //trocar letras por imagens
 
-function joga(celula){
+ function joga(celula){
     celulaclicada = document.getElementById(celula).innerHTML;
    if (celulaclicada == "<img src = ankh.png>" || celulaclicada == "<img src = horus.png>"){
        alert("Aqui não!");
    }else{
         document.getElementById(celula).innerHTML = icone;
         if (icone == "<img src = ankh.png>"){
-
             icone = "<img src = horus.png>";
-
         }else{
             icone = "<img src = ankh.png>";
         }
    }
 }
+
 //verifica-chama o bloco escolhido
 function verif(){
    a1 = document.getElementById('a1').innerHTML;
@@ -37,12 +36,11 @@ function verif(){
         (c1 != '') && (c2 != '') && (c3 != '') && (c1 == c2) && (c2 == c3)) || (
         (a2 != '') && (b2 != '') && (c2 != '') && (a2 == b2) && (b2 == c2)) || (
         (a3 != '') && (b3 != '') && (c3 != '') && (a3 == b3) && (b3 == c3)) || (
-        (c1 != '') && (b2 != '') && (a3 != '') && (c1 == b2) && (b2 == a3)) // acho que deve ter um jeito mais simples de fazer isso :(
+        (c1 != '') && (b2 != '') && (a3 != '') && (c1 == b2) && (b2 == a3)) 
+        // acho que deve ter um jeito mais simples de fazer isso :(
     ){
 
        alert('Parece que alguém ganhou!');
-       novo();
-
     }
 }
  
